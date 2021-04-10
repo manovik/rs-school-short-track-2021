7b0db8d5b78e7d15d75f39074a6c4305e7124e51
Maxim Novikov <maxnovbel@gmail.com>
2021-04-10 14:16:30 +0300

    feat: add 04 solution idea

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,48 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  "use strict";
+  const arr = [];
+
+  matrix.forEach(el => {
+    arr.push(Array());
+  })
+  
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      arr[i].push(count(matrix, i, matrix[j], j))
+    }
+  }
+
+  function count(el, arrIdx, arr, elIdx) {
+    let sum = 0;
+    if(arr[arrIdx - 1]) {
+      sum += arr[arrIdx - 1] ? 1 : 0;
+    }
+    if(arr[arrIdx + 1]) {
+      sum += arr[arrIdx + 1] ? 1 : 0;
+    }
+    if(el[elIdx - 1]) {
+      sum += el[elIdx - 1] ? 1 : 0;
+    }
+    if(el[elIdx + 1]) {
+      sum += el[elIdx + 1] ? 1 : 0;
+    }
+    return sum;
+  }
+
+  return arr;
 }
 
+console.log(minesweeper([
+ [true, false, false, false],
+ [false, true, true, false],
+ [false, true, true, false],
+ [false, false, false, false]
+]))
+
 module.exports = minesweeper;
+
+// 1 2 1 1
+// 2 2 2 1
