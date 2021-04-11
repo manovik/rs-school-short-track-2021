ad2db2bb3252b0707e1abb2ec7f0958670c8c70a
Maxim Novikov <maxnovbel@mail.ru>
2021-04-11 12:43:25 +0300

    feat: add solvation 09

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,16 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let j = n;
+
+  if (j < 10) {
+    return j;
+  }
+  const count = (num) => String(num).split('').reduce((acc, m) => +acc + +m);
+
+  j = count(j);
+  return getSumOfDigits(j);
 }
 
 module.exports = getSumOfDigits;
