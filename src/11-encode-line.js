2c3b6930e156463857d9425d6ac6e72bc8e33cf1
Maxim Novikov <maxnovbel@mail.ru>
2021-04-11 16:01:27 +0300

    feat: add solvation 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let finalStr = '';
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < str.length;) {
+      if (str[i] === str[j]) {
+        count += 1;
+        j++;
+      } else {
+        finalStr += count > 1 ? `${count}${str[i]}` : str[i];
+        i = j;
+        count = 0;
+      }
+    }
+    finalStr += count > 1 ? `${count}${str[i]}` : str[i];
+    count = 0;
+    break;
+  }
+  return finalStr;
 }
 
 module.exports = encodeLine;
