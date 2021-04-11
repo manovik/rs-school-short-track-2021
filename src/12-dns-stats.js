2d32a1dabdffbbd8c2a69f0b1d7319fd85fea476
Maxim Novikov <maxnovbel@mail.ru>
2021-04-11 23:37:10 +0300

    feat: add solvation 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,32 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const obj = {};
+
+  domains.forEach((d) => {
+    const rev = d.split('.').reverse();
+
+    while (rev.length > 0) {
+      const w = `.${rev.join('.')}`;
+      if (Object.keys(obj).includes(w)) {
+        obj[w] += 1;
+      } else {
+        obj[w] = 1;
+      }
+      rev.pop();
+    }
+  });
+
+  return obj;
 }
+
+// const a = [
+//   'code.yandex.ru',
+//   'music.yandex.ru',
+//   'yandex.ru',
+// ];
+
+// console.log(getDNSStats(a));
 
 module.exports = getDNSStats;
