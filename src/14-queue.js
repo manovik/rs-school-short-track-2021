a415f5c9b96c70d6f524812dcccadba2700b512e
Maxim Novikov <maxnovbel@mail.ru>
2021-04-12 01:08:03 +0300

    feat: add solvation 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,17 +12,71 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.length;
+  }
+
+  enqueue(element) {
+    const newEl = new ListNode(element);
+    newEl.next = this.head;
+    this.head = newEl;
+    this.length += 1;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    let cur = this.head;
+    let prev = cur;
+    while (cur.next) {
+      prev = cur;
+      cur = cur.next;
+    }
+    prev.next = cur.next;
+    this.length -= 1;
+    return cur.value;
   }
 }
+
+// const queue = new Queue();
+// queue.enqueue(5);
+// console.log(queue.dequeue());
+// queue.dequeue();
+// queue.enqueue(5);
+// queue.enqueue(6);
+// queue.enqueue(7);
+// console.log(queue.dequeue());
+// console.log(queue.dequeue());
+// queue.enqueue(9);
+// console.log(queue.dequeue());
+// queue.enqueue(15);
+// queue.enqueue(16);
+// queue.enqueue(17);
+// console.log(queue.dequeue());
+// console.log(queue);
+
+// // const queue = new Queue();
+// // queue.enqueue(1); // adds the element to the queue
+// // queue.enqueue(3); // adds the element to the queue
+// // queue.enqueue(5); // adds the element to the queue
+// // queue.enqueue(7); // adds the element to the queue
+// // queue.enqueue(9); // adds the element to the queue
+// // // queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+// // console.log('🚀 ~ file: 14-queue.js ~ line 46 ~ queue.dequeue()', queue.dequeue());
+
+// // dequeue() {
+// //   let prev = null;
+// //   let cur = this.head;
+// //   while (cur.next) {
+// //     prev = cur;
+// //     cur = cur.next;
+// //   }
+// //   prev.next = null;
+// //   this.length -= 1;
+// //   return cur.value;
+// // }
 
 module.exports = Queue;
