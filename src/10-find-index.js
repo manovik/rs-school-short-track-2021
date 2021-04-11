b7ad4189314fb02e8af824fc1e729a368727b09a
Maxim Novikov <maxnovbel@mail.ru>
2021-04-11 15:14:29 +0300

    feat: add solvation 10 (time failing?)

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,30 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let i = 0;
+
+  const find = (arr, val) => {
+    const arrLeft = arr.slice(0, Math.ceil(arr.length / 2));
+    const arrRight = arr.slice(Math.ceil(arr.length / 2));
+
+    if (arrLeft[arrLeft.length - 1] > val) {
+      i = arrLeft.length - 1;
+      return find(arrLeft, val);
+    }
+    if (arrRight[0] < val) {
+      i += arrLeft.length;
+      return find(arrRight, val);
+    }
+    if (arrLeft[arrLeft.length - 1] === val) {
+      i = arrLeft.length - 1;
+    }
+    if (arrRight[0] === val) {
+      i += arrLeft.length;
+    }
+    return i;
+  };
+  return find(array, value);
 }
 
 module.exports = findIndex;
